e34e436398a5ac6050da85674a94bb1030353933
raihaneka50908 <61652645+raihaneka50908@users.noreply.github.com>
2020-08-08 22:09:56 +0700

    Add files via upload

---
+++ Credits_To_Minute_Meals.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin();
+  lcd.backlight();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(0,0);
+  lcd.print("@minute.mealss");
+}
